88642154929f8fad1fc40e06fee821673734ef5a
jfialkowski <50218632+jfialkowski@users.noreply.github.com>
2019-05-16 14:10:19 -0400

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello World</h1>")
+	fmt.Fprintf(w, "<h1>Hello From Pipeline!!!</h1>")
 }
 
 func about(w http.ResponseWriter, r *http.Request) {
