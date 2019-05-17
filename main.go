df108aad49199bb8937b72f3cfd32cf49e5326ee
jfialkowski <noreply@prgrmr.io>
2019-05-17 15:33:01 -0400

    Added Contact US

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 )
 
-
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello Updated!!!</h1>")
 }
@@ -14,9 +13,14 @@ func about(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>About</h1>")
 }
 
+func contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Contact</h1>")
+}
+
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/about", about)
+	http.HandleFunc("/contactus", contact)
 	fmt.Println("Server Starting")
 	http.ListenAndServe(":3000", nil)
 }
