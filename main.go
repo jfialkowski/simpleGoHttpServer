f9594f26d3e27472f9f6bdeb7c41aba314252c73
jfialkowski <noreply@prgrmr.io>
2019-05-20 14:35:54 -0400

    :Updated

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello Updated!!!</h1>")
+	fmt.Fprintf(w, "<h1>Hello World!!!</h1>")
 }
 
 func about(w http.ResponseWriter, r *http.Request) {
